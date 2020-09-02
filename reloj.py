eb858b0fdc3711d3fd03a47b5fe1db90f0627a5a
Jesus <33559839+Kaiser16@users.noreply.github.com>
2020-09-02 18:29:54 +0200

    Add files via upload

---
+++ reloj.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import datetime
+import time
+import winsound
+import os
+
+
+while True:
+        abierto = False
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        if(current_time.hour%2 != 0 and current_time.minute == 55 and abierto == False):
+            os.startfile(r"C:\Users\black\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Nostale.url")
+            abierto = True
+            time.sleep(59)
+        elif(abierto):
+            abierto = False
+
