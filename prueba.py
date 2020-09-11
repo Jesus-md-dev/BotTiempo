ba491777a7b5ada3ec432651b9eba1fbd3bcb93e
Kaiser16 <jesusmarquezdelgado@gmail.com>
2020-09-11 12:56:22 +0200

    th

--- prueba.py
+++ prueba.py
@@ -1,53 +1,52 @@
-import datetime
+import threading
 import time
-import winsound
-import os
-from tkinter import *
+import tkinter as tk
 
-class Application(Frame):
-    def say_hi(self):
-        print ("hi there, everyone!")
-    
-    def timer(self):
-        while True:
-        abierto = False
-        time.sleep(1)
-        current_time = datetime.datetime.now()
-        if(current_time.hour%2 != 0 and current_time.minute == 55 and abierto == False):
-            os.startfile(r"C:\Users\black\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Nostale.url")
-            abierto = True
-            time.sleep(59)
-        elif(abierto):
-            abierto = False
-
-    def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "black"
-        self.QUIT["command"] =  self.quit
-
-        self.QUIT.pack({"side": "left"})
-
-        self.evenh = Button(self)
-        self.evenh["text"] = "Even Hour",
-        self.QUIT["fg"]   = "red"
-        self.evenh["command"] = self.evenh
-
-        self.evenh.pack({"side": "left"})
-
-        self.oddh = Button(self)
-        self.oddh["text"] = "Odd Hour",
-        self.QUIT["fg"]   = "red"
-        self.oddh["command"] = self.oddh
-
-        self.oddh.pack({"side": "left"})
-
+class Application(tk.Frame):
     def __init__(self, master=None):
-        Frame.__init__(self, master)
+        super().__init__(master)
+        self.i = 0
+        self.sem = threading.Semaphore()
+        self.master = master
         self.pack()
-        self.createWidgets()
+        self.create_widgets()
 
-root = Tk()
+    def fun1(self):
+        self.sem.acquire()
+        print(self.i)
+        self.i = (self.i+1)%2
+        print(self.i)
+        self.sem.release()
+        time.sleep(0.25)
+
+    def fun2(self):
+        j = 0
+        while j < 10:
+            self.sem.acquire()
+            print(self.i)
+            self.sem.release()
+            time.sleep(0.25)
+            j+=1
+
+    def create_widgets(self):
+        self.Start = tk.Button(self)
+        self.Start["text"] = "Start"
+        self.Start["command"] = self.fun2
+        self.Start.pack(side="left")
+
+        self.Change = tk.Button(self)
+        self.Change["text"] = "Change"
+        self.Change["command"] = self.fun1
+        self.Change.pack(side="right")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
-root.destroy()
+
